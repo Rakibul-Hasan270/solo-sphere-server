7077708d69db13206b020ee2ee7752bdd868303b
Rakibul-Hasan270 <rakibx270@gmail.com>
2025-04-22 18:18:16 +0600

    create some api, implement jwt token and remove jwt token

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
 
 
 
@@ -29,9 +36,30 @@ async function run() {
         const jobsCollection = client.db('soloSphereDB').collection('jobs');
         const bidCollection = client.db('soloSphereDB').collection('bids');
 
+        // jwt token 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            }).send({ success: true })
+        })
+
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                maxAge: 0
+            }).send({ success: true })
+        })
+
+
         app.get('/jobs', async (req, res) => {
             const result = await jobsCollection.find().toArray();
-            console.log(result)
+            // console.log(result)
             res.send(result)
         })
 
@@ -92,18 +120,41 @@ async function run() {
             res.send(result);
         })
 
-        app.put('/update/:id', async (req, res) => {
-            const id = req.params.id;
-            const jobBody = req.body;
+        // app.put('/update/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const jobBody = req.body;
 
-        })
+        // })
 
+        // akta user email (property) diye database theke data neyha 
         app.get('/myBids/:email', async (req, res) => {
             const email = req.params.email;
             const filter = { email };
             const result = await bidCollection.find(filter).toArray();
             res.send(result);
         })
+
+        app.get('/bidRequest/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { 'buyer.email': email };
+            const result = await bidCollection.find(filter).toArray();
+            res.send(result);
+        })
+
+        // bid status update  
+        app.patch('/bid-status/:id', async (req, res) => {
+            const id = req.params.id;
+            const currentStatus = req.body;
+            // console.log(id, currentStatus);  //  { status: 'In Progress' }
+            const query = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: { ...currentStatus }
+            }
+            const result = await bidCollection.updateOne(query, updateDoc);
+            console.log(result)
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
